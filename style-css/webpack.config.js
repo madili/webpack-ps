3cdc4e3f4fe3bda5de7c829689d9316a36fc54a0
Marcos Dioni Lima <mlima@vix.itextreme.com.br>
2016-06-02 18:22:37 -0300

    css styles

---
+++ style-css/webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    context: path.resolve('js'),
+    entry: ["./app"],
+    output: {
+        path: path.resolve('build/'),
+        publicPath: '/public/assets/',
+        filename: "bundle.js"
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ],
+    devServer: {
+        contentBase: 'public'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader")
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!less-loader")
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.es6']
+    }
+}
